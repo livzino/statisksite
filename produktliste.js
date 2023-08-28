2cb801714419910a0fbdab57e20fb0ec917be5e4
Liv <livbendix@live.dk>
2023-08-28 14:56:11 +0200

    jsproduktliste

---
+++ produktliste.js
@@ -0,0 +1,50 @@
+fetch("https://kea-alt-del.dk/t7/api/products")
+  .then((res) => res.json())
+  .then(showProducts);
+
+function showProducts(products) {
+  products.forEach(showProduct);
+}
+
+function showProduct(product) {
+  //console.log(product);
+  const template = document.querySelector("#smallproducttemplate").content;
+
+  const copy = template.cloneNode(true);
+  copy.querySelector("h3").textContent = product.productdisplayname;
+  copy.querySelector("p.subtle").textContent = product.articletype;
+  copy.querySelector("p.price").textContent = product.price;
+
+  copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldOut");
+  }
+  if (product.discount) {
+    copy.querySelector("article").classList.add("discounted");
+  }
+  document.querySelector(".produktgrid").appendChild(copy);
+}
+/*
+id: 1163, 
+gender: "Men", 
+category: "Apparel", 
+subcategory: "Topwear", 
+articletype: "Tshirts", 
+season: "Summer", 
+productionyear: 2011, 
+usagetype: "Sports", 
+productdisplayname: "Sahara Team India Fanwear Round Neck Jersey", 
+price: 895, … 
+
+    <article class="smallproduct">
+                <img src="https://kea-alt-del.dk/t7/images/webp/640/1573.webp" alt="bukser">
+                <h3>Sahara Team India Fanwear Round Neck Jersey</h3>
+                <p class="subtle">Tshirts | Nike</p>
+                <p class="price"><span>Prev.</span> DKK 1595,-</p>
+                <div class="discounted">
+                    <p>Now: 1000</p>
+                    <p>-35%</p>
+                </div>
+                <a href="produkt.html">READ MORE</a>
+            </article>
+            */
